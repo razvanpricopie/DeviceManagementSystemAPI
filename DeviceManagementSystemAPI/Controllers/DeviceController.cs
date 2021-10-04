d7dc0c21952645bb37cb9176139efc90a0cd6452
Razvan Pricopie <razvan@thecodecrowd.nl>
2021-10-04 20:06:02 +0300

    name fix

---
+++ DeviceManagementSystemAPI/Controllers/DeviceController.cs
@@ -0,0 +1,31 @@
+﻿using Contracts;
+using Entities.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeviceManagementSystemAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeviceController : ControllerBase
+    {
+        private readonly IRepositoryWrapper _repositoryWrapper;
+
+        public DeviceController(IRepositoryWrapper repositoryWrapper)
+        {
+            _repositoryWrapper = repositoryWrapper;
+        }
+
+        [HttpGet]
+        public IEnumerable<Device> GetDevices()
+        {
+            return _repositoryWrapper.Device.FindAll();
+        }
+
+    }
+}
